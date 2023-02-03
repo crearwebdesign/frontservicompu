190fd93b7744c9589d35f0748237cb7f70254c3f
crearwebdesign <oscar10034@gmail.com>
2023-02-03 19:09:43 -0400

    End-MostraUsua

--- src/app/services/busquedas.service.ts
+++ src/app/services/busquedas.service.ts
@@ -2,6 +2,7 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { map } from 'rxjs/operators';
 import { environment } from 'src/environments/environment';
+import { Usuario } from '../models/usuario.model';
 
 const base_url = environment.base_url;
 
@@ -24,6 +25,13 @@ export class BusquedasService {
     }
 };
 
+private transformarUsuarios( resultados : any[]) : Usuario[] {
+  return resultados.map(
+    user => new Usuario( user.nombre, user.email, '', user.img, user.google,user.role,user.uid )
+  )
+
+};
+
 buscar (
         tipo : 'usuarios' | 'medicos' | 'hospitales',
         termino : string
@@ -32,7 +40,16 @@ buscar (
         const url = `${base_url}/todo/coleccion/${tipo}/${termino}`; //la busquedad en el backend
         return this.http.get<any[]>(url, this.headers)
                    .pipe(
-                    map( (resp : any ) => resp.resultado)
+                    map( (resp : any ) => {
+                      switch (tipo) {
+                        case 'usuarios':
+                          return this.transformarUsuarios( resp.resultado )
+                          break;
+                      
+                        default:
+                          return[];
+                      }
+                    })
                    )
 
 }
