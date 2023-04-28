34d9162e46f241c36a41d1196885968c9f02b2ae
crearwebdesign <oscar10034@gmail.com>
2023-04-28 16:24:28 -0400

    CrearModeloServiciosdeHospitales

---
+++ src/app/services/hospital.service.ts
@@ -0,0 +1,38 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { map } from 'rxjs/operators';
+
+import { environment } from 'src/environments/environment';
+import { Hospital } from '../models/hospital.models';
+
+const base_url = environment.base_url;
+
+@Injectable({
+  providedIn: 'root'
+})
+export class HospitalService {
+
+  constructor( private http : HttpClient) {}
+
+  get token(): string {
+    return localStorage.getItem('token') || '';
+  };
+
+    get headers(){
+      return {
+        headers : {
+          'x-token': this.token
+        }
+      }
+  };
+
+  cargarHospitales(){
+    const url = `${base_url}/hospitales`; //la busquedad en el backend
+    return this.http.get(url, this.headers).
+           pipe(
+            map( (resp : {ok : boolean, hospitales : Hospital[]} ) =>  resp.hospitales)
+           )
+
+  };
+
+}
