75875ca4647bc38f5acb5b4af0e8c76db19133c3
Oscar Barrios <oscar10034@gmail.com>
2023-05-01 16:02:05 -0400

    MostrarHospitales

--- src/app/pages/mantenimientos/hospitales/hospitales.component.ts
+++ src/app/pages/mantenimientos/hospitales/hospitales.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Hospital } from 'src/app/models/hospital.models';
 import { HospitalService } from 'src/app/services/hospital.service';
 
 @Component({
@@ -9,14 +10,23 @@ import { HospitalService } from 'src/app/services/hospital.service';
 })
 export class HospitalesComponent implements OnInit {
 
+  public hospitales :  Hospital[] = [];
+  public cargando : boolean = true;
+
   constructor( private hospitalService : HospitalService) { }
 
   ngOnInit(): void {
+    this.cargarHospitales()
+  };
+  
+  cargarHospitales(){
+    this.cargando = true;
     this.hospitalService.cargarHospitales()
         .subscribe( hospitales => {
-          console.log(hospitales)
+          this.cargando = false;
+          this.hospitales = hospitales
         })
-
+    
   }
 
 }
