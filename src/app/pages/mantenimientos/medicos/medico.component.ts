a28075db6a690e34357292c1d861515610fc6c1b
crearwebdesign <oscar10034@gmail.com>
2023-06-17 17:18:48 -0400

    PrepararMedico

--- src/app/pages/mantenimientos/medicos/medico.component.ts
+++ src/app/pages/mantenimientos/medicos/medico.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Hospital } from 'src/app/models/hospital.models';
+import { HospitalService } from 'src/app/services/hospital.service';
 
 @Component({
   selector: 'app-medico',
@@ -8,9 +11,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MedicoComponent implements OnInit {
 
-  constructor() { }
+  public medicoForm : FormGroup;
+  public hospitales : Hospital[] = [];
+
+  constructor( private fb : FormBuilder,
+               private hospitalService : HospitalService) { }
 
   ngOnInit(): void {
+    this.medicoForm = this.fb.group({
+      nombre : ['Oscar Barrios',Validators.required],
+      hospital : ['',Validators.required]
+    });
+    this.cargarHospitales()
+  };
+
+  cargarHospitales(){
+    this.hospitalService.cargarHospitales()
+        .subscribe( ( hospitales : Hospital[]) => {
+          this.hospitales = hospitales
+        } )
+  };
+
+  guardarMedico(){
+    console.log(this.medicoForm.value)
   }
 
 }
